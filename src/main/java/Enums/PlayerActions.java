9bbb5b1dcc433d2108123ab22fbd4b2c3d4b3ced
louis <louiscaesapdf@gmail.com>
2023-02-06 20:55:46 +0700

    revisi enums

--- src/main/java/Enums/PlayerActions.java
+++ src/main/java/Enums/PlayerActions.java
@@ -4,7 +4,13 @@ public enum PlayerActions {
   FORWARD(1),
   STOP(2),
   START_AFTERBURNER(3),
-  STOP_AFTERBURNER(4);
+  STOP_AFTERBURNER(4),
+  FIRE_TORPEDOES(5),
+  FIRE_SUPERNOVA(6),
+  DETONATE_SUPERNOVA(7),
+  FIRE_TELEPORTER(8),
+  TELEPORT(9),
+  USE_SHIELD(10);
 
   public final Integer value;
 
