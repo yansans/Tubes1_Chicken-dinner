5cbc529da6dd0a05f977af48e95e76488f060126
louis <louiscaesapdf@gmail.com>
2023-02-09 02:29:09 +0700

    defense priority sudah, retreat untuk supernova dan gas cloud sudah, retreat dari kejaran player masih diconsider

--- src/main/decision/decisionmaker/decisionmaker.java
+++ src/main/decision/decisionmaker/decisionmaker.java
@@ -10,6 +10,7 @@ import java.util.stream.*;
 
 public class decisionmaker {
     public int decision_kind;
+    public int decision_kind_variation;
 
     public defense defense_prio;
     public retreat retreat_prio;
@@ -28,11 +29,28 @@ public class decisionmaker {
 
     public PlayerAction whatBotShouldDo(GameObject player) {
         findPriority(player);
+        PlayerActions command;
 
         if (this.decision_kind == 1) { // anggap 1 adalah defense
-            return PlayerActions.USE_SHIELD;
+            command.action = PlayerActions.USE_SHIELD;
+            return command;
         } else if (this.decision_kind == 2) { // anggap 2 adalah retreat
-            
+            if (decision_kind_variation == 1) { // lari dari gas
+                // putar balik
+                command.action = PlayerActions.FORWARD;
+                command.heading = (player.currentHeading + 180) % 360;
+            } else if (decision_kind_variation == 2) { // lari dari player lain
+
+            } else { // lari dari supernova
+                // menjauh dari tempat supernova sekarang
+                var supernovalist = gameState.getGameObjects()
+                .stream().filter(item -> item.getGameObjectType() == ObjectTypes.SUPER_NOVA_BOMB)
+                .sorted(Comparator.comparing(item -> distanceFromPlayerToProjectileTrajectory(item, player)))
+                .collect(Collectors.toList());
+
+                command.action = PlayerActions.FORWARD;
+                command.heading = (objectHeading(supernovalist.get(0), player) + 180) % 360;
+            }
         }
     }
 }
