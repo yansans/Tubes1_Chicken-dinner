cdba1a538c0f5ac4142732c19c11cdf821411472
yansans <66671259+yansans@users.noreply.github.com>
2023-02-10 18:59:18 +0700

    update function , need refactoring

--- src/main/java/Services/Offence.java
+++ src/main/java/Services/Offence.java
@@ -6,12 +6,54 @@ import Models.*;
 import java.util.*;
 import java.util.stream.*;
 
+
+// timestamp
+// 32:38
+// 34:55
+// 35:16
+// 1:42:22
+// 1:49:00
+
+// prio attack
+// eat using tp
+// fire supernova (depends on situation)
+// fire torpedos (when size identical effective)
+
+// circumtances
+// asteroid - torpedoes 
+// wormhole - projectiles
+
+// when losing (size smallest) try to collect point from food and wormhole
 public class Offence extends BotService {
 
     private int supernovaSize = 10;
     private double supernovaRadius = 0.25 * supernovaSize;
 
-    private List<GameObject> getObjectList(ObjectTypes type){
+    public Offence(BotService b){
+        setBot(getBot());
+        setPlayerAction(getPlayerAction());
+    }
+
+    public void setOffence(){
+        setBot(getBot());
+        setPlayerAction(getPlayerAction());
+    }
+
+    private List<GameObject> distanceFrom(GameObject object, ObjectTypes type){
+        // membuat list gameobject tersusun terdekat dari suatu object
+        var gameState = getGameState();
+        
+        var orderedList = gameState.getGameObjects()
+                .stream().filter(item -> item.getGameObjectType() == type)
+                .sorted(Comparator
+                        .comparing(item -> getDistanceBetween(object, item)))
+                .collect(Collectors.toList());
+
+        return orderedList;
+    }
+    
+    private List<GameObject> getObjectListDistance(ObjectTypes type){
+        // membuat list gameobject tersusun dari yang terdekat terhadap player
         var gameState = getGameState();
         var bot = getBot();
 
@@ -24,7 +66,22 @@ public class Offence extends BotService {
         return objectList;
     }
 
+    private List<GameObject> getObjectListSize(ObjectTypes type){
+        // membuat list gameobject tersusun dari yang terkecil 
+        var gameState = getGameState();
+        var bot = getBot();
+
+        var objectList = gameState.getGameObjects()
+                .stream().filter(item -> item.getGameObjectType() == type)
+                .sorted(Comparator
+                        .comparing(item -> item.getSize()))
+                .collect(Collectors.toList());
+
+        return objectList;
+    }
+
     public void commandPlayer(PlayerActions command, int head){
+        // menset playeraction dari command dan head
         var playerAction = getPlayerAction();
         playerAction.action = command;
         playerAction.heading = head;
@@ -33,25 +90,58 @@ public class Offence extends BotService {
     }
 
     private List<Object> defaultAction(){
-        var action = PlayerActions.FORWARD;
+        // return list of playeraction default yaitu Forward ke head makanan atau 0
+        var command = PlayerActions.FORWARD;
         int head = 0;
 
-        var playerList = getObjectList(ObjectTypes.FOOD);
+        var playerList = getObjectListDistance(ObjectTypes.FOOD);
 
         if (!playerList.isEmpty()) {
             head = getHeadingBetween(playerList.get(0));
         } 
 
-        return Arrays.asList(action, head);
+        return Arrays.asList(command, head);
     }
 
 
-    public void basicAttack(PlayerActions action, ObjectTypes object){
+    public void basicAttackDistance(PlayerActions command, ObjectTypes object, boolean desc){
+        // melakukan command dengan heading objek dengan jarak tertentu dari player
+        // jika true jarak terdekat false jarak terjauh
+
         int head = 0;
-        var objectList = getObjectList(object);
+        var objectList = getObjectListDistance(object);
 
         if(object == ObjectTypes.PLAYER){
             objectList.remove(0);
+        }
+
+        if (desc) {
+            Collections.reverse(objectList);
+        }
+
+        if (!objectList.isEmpty()) {
+            head = getHeadingBetween(objectList.get(0));
+        } else {
+            var defaultAction = defaultAction();
+            command = (PlayerActions) defaultAction.get(0);
+            head = (int) defaultAction.get(1);
+        }
+        commandPlayer(command, head);
+    }
+
+    public void basicAttackSize(PlayerActions action, ObjectTypes object, boolean desc){
+        // melakukan command dengan heading objek dengan size tertentu dari player
+        // jika true size terkecil false size terbesar
+
+        int head = 0;
+        var objectList = getObjectListSize(object);
+
+        if(object == ObjectTypes.PLAYER){
+            objectList.remove(0);
+        }
+
+        if (desc) {
+            Collections.reverse(objectList);
         }
 
         if (!objectList.isEmpty()) {
@@ -64,25 +154,42 @@ public class Offence extends BotService {
         commandPlayer(action, head);
     }
 
+    // bisa pake fungsi default
+    public void fireTelport(GameObject player){
+        // tembak teleport ke player
+        var command = PlayerActions.FIRE_TELEPORTER;
+        int head = getHeadingBetween(player);
+
+        commandPlayer(command, head);
+    }
+
+    public void playerTeleport(){
+        // melakukan teleport
+        commandPlayer(PlayerActions.FIRE_TELEPORTER, 0);
+    }
+
     public void detonateSupernova(){
-        var action = PlayerActions.DETONATE_SUPERNOVA;
+        // meledakan supernova jika player memasuki radius ledakan
+        var command = PlayerActions.DETONATE_SUPERNOVA;
         int head = 0;
 
-        var playerList = getObjectList(ObjectTypes.PLAYER);
+        var playerList = getObjectListDistance(ObjectTypes.PLAYER);
         playerList.remove(0);
 
-        var supernovaBombList = getObjectList(ObjectTypes.SUPER_NOVA_BOMB);
+        var supernovaBombList = getObjectListDistance(ObjectTypes.SUPER_NOVA_BOMB);
 
         if (!playerList.isEmpty() && !supernovaBombList.isEmpty()) {
             if (getDistanceBetween(playerList.get(0), supernovaBombList.get(0)) > supernovaRadius) {
-                action = PlayerActions.FORWARD;
-                head = 0;
+                var defaultAction = defaultAction();
+                command = (PlayerActions) defaultAction.get(0);
+                head = (int) defaultAction.get(1);
             }
         }
-        commandPlayer(action, head);
+        commandPlayer(command, head);
     }
 
     private int predictHead(GameObject projectile, GameObject player){
+        // memprediksi heading yang dibutuhkan untuk menembak ke tempat player akan bergerak
 
         int projectile_speed = projectile.getSpeed();
         int player_speed = player.getSpeed();
@@ -100,6 +207,11 @@ public class Offence extends BotService {
         double heading = Math.atan2(y - bot.getPosition().y, x - bot.getPosition().x);
 
         return (int) Math.toDegrees(heading);
+    }
+
+    private int tickFromDistance(GameObject projectile, GameObject player){
+        // menghitung tick yang dibutuhkan projectile untuk menuju player
+        return (int) getDistanceBetween(projectile, player) / projectile.getSpeed();
     }
 
 }
