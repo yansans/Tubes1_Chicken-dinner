7e0f6750526196afba989673b6ee4d1cf2e20c36
louis <louiscaesapdf@gmail.com>
2023-02-15 20:33:59 +0700

    fix main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -35,7 +35,7 @@ public class Main {
             System.out.println("Registered with the runner " + id);
 
             Position position = new Position();
-            GameObject bot = new GameObject(id, 10, 20, 0, position, ObjectTypes.PLAYER);
+            GameObject bot = new GameObject(id, 10, 20, 0, position, ObjectTypes.PLAYER, 0, 0, 0, 0, 0, 0);
             botService.setBot(bot);
         }, UUID.class);
 
